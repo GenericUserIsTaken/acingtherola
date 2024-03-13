d3b03ae4957d4846579a5110303d0744cc8c003d
GenericUserIsTaken <65835587+GenericUserIsTaken@users.noreply.github.com>
2024-03-13 15:34:02 -0700

    tmime

---
+++ Time.gd
@@ -0,0 +1,33 @@
+extends Label
+
+var hours = 0
+var min = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func makeDouble(num):
+	if num > 9:
+		return str(num)
+	else:
+		return "0"+str(num)
+
+func getText():
+	return "TIME: " + makeDouble(hours) + ":"+ makeDouble(min)
+
+func addMin():
+	if min >= 60:
+		min = 0
+		hours+=1
+	else:
+		min +=1
+
+func _on_timer_timeout():
+	addMin()
+	self.text = getText()
--- termcmd.gd
+++ termcmd.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal responsetxt(out)
+var commandToMethod = {"help" : help, "cmds" : cmds,"thrust" : thrust}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,5 +12,27 @@ func _process(delta):
 	pass
 
 func cmd_entered(cmd):
-	#check if valid cmd
-	responsetxt.emit("you entered a stupid ass command")
+	var splitcmd = cmd.split(" ") # "  " is allowed
+	if splitcmd[0] in commandToMethod:
+		commandToMethod[splitcmd[0]].call(splitcmd)
+	else:
+		responsetxt.emit("no such command detected")
+
+func pr(texts):
+	if typeof(texts) == TYPE_ARRAY or typeof(texts) == TYPE_PACKED_STRING_ARRAY:
+		var out = ""
+		for text in texts:
+			out+= text
+			out += " "
+		responsetxt.emit(out)
+	else:
+		responsetxt.emit(str(texts))
+	
+func help(cmd):
+	pr(cmd)
+
+func thrust(cmd):
+	pr("real")
+
+func cmds(cmd):
+	pr("cmd")
